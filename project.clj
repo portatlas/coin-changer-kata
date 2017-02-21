3b27718ee39f676b43a4365e65fc1939ff6f9ab7
portatlas <a.vathanaka@gmail.com>
2017-02-20 16:22:12 -0800

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject proto-repl-demo "0.1.0-SNAPSHOT"
-  :description "A sample Clojure project for demoing capabilities of the Proto REPL package for the Atom editor."
-  :url "https://github.com/jasongilman/proto-repl-demo"
+(defproject coin-changer-kata "0.1.0-SNAPSHOT"
+  :description "Coin Changing App in Clojure"
+  :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.8.0"]
